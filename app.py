fce05f5d9ee7b6f9f18550c6d6bd837525460ff4
itsluckysharma01 <panditluckysharma42646@gmail.com>
2025-07-04 22:27:55 +0530

    Update App.py

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def predict():
         return render_template('result.html', prediction="Error: Could not make prediction", error=error_message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
